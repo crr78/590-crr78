69ca1ea2ad9e2f8b722f412e85ca9d2569800d40
crr78 <crr78@georgetown.edu>
2021-09-24 17:06:25 -0400

    Starting HW3.0

---
+++ HW3.0/HW3.0A2.py
@@ -0,0 +1,6 @@
+"""
+Chevy Robertson (crr78@georgetown.edu)
+Neural Nets & Deep Learning
+HW3.0A2: Code Binary Classification Using Keras, Train on IMDB Dataset
+10/05/2021
+"""
---
+++ HW3.0/HW3.0A3.py
@@ -0,0 +1,6 @@
+"""
+Chevy Robertson (crr78@georgetown.edu)
+Neural Nets & Deep Learning
+HW3.0A3: Code Multi-Class Classification Using Keras, Train on Newswire Dataset
+10/05/2021
+"""
